890a2c204e55de11056bfeb0727a2c5f624d08d4
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-07-18 20:54:56 +0300

    Patika.dev | C# Tip Dönüşümleri

---
+++ Tip Dönüşümleri/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Tip_D_n___mleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion (Bilinçsiz Dönüşüm)
+            Console.WriteLine("***** Implicit Conversion *****");
+
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d  = a+b+c; //integer'in kapasitesi daha fazla
+            Console.WriteLine("d:" + d);
+
+            long h = d; // long'un kapasitesi integer'dan daha fazladır
+            Console.WriteLine("h:" + h);
+
+            float i = h;
+            Console.WriteLine("i" + i);
+
+            string e = "kübra";
+            char f = 'k';
+            object g = e + f + d; //daha kapasiteli
+            Console.WriteLine("g:" + g);
+
+
+            // Explicit  Conversion (Bilinçsiz Dönüşüm)
+
+            Console.WriteLine("***** Explicit Conversion *****");
+
+            int x = 4;
+            byte y = (byte)x; // byte int'den daha küçük kapasiteli, cast edilir.
+                              // Parantez içine hedef veri tipi yazılır;  
+                              // sağındaki veriyi, parantez içindeki veri tipine dönüştürür.
+            Console.WriteLine("y:" +y);
+
+            int z = 100;
+            byte t = (byte)z;
+            Console.WriteLine("t:" +t);
+
+            float w = 10.3f;
+            byte v = (byte)w;
+            Console.WriteLine("v:" +v);
+
+            // *** ToString Metodu ***
+            //Aldığı veriyi string'e cast eder.
+
+            Console.WriteLine("*** ToString Metodu ***");
+            int xx = 6;
+            string yy = xx.ToString();
+            Console.WriteLine("yy:" +yy);
+
+            string zz = 12.5f.ToString();
+            Console.WriteLine("zz:" +zz);
+
+            //System.Convert
+            Console.WriteLine("*** System.Convert Sınıfı ***");
+            string s1 ="10", s2 ="20";
+            int sayi1, sayi2;
+            int toplam;
+
+            sayi1 = Convert.ToInt32(s1);
+            sayi2 = Convert.ToInt32(s2);
+
+            toplam = sayi1 + sayi2;
+            Console.WriteLine("toplam:" +toplam);
+
+            //Parse
+            Console.WriteLine("***** Parse Methodu *****");
+            ParseMethod();
+        }
+
+        public static void ParseMethod()
+        {
+            string metin1 = "10";
+            string metin2 = "10,25";
+            int rakam1;
+            double double1;
+
+            rakam1 = Int32.Parse(metin1); //string bir değişken parse kullanarak integer bir değişkene çevrildi.
+            double1 = Double.Parse(metin2);
+
+            // Parse, string ifadeleri dönüştürmek için kullanılır ve her zaman string alır.
+
+            Console.WriteLine("rakam1 :" +rakam1);
+            Console.WriteLine("double1 :" +double1);
+
+        }
+    }
+}
