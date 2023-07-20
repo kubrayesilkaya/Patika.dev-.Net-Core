be8e6198213f4c487ef191dd783b75e8f2b4b1d7
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-07-20 15:42:17 +0300

    Patika.dev | C# Switch-Case

---
+++ C# Switch-Case/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Espression
+            switch (month)
+            {
+                case 1: 
+                    Console.WriteLine("Ocak Ayındasınız!");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız!");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız");
+                    break ;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız!");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girdiniz!"); 
+                break;
+            }
+
+            // Birden fazla case ifadesi tek bir kod bloğunu çalıştıracak şekilde yazılabilir:
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                  Console.WriteLine("Kış ayındasınız!");
+                  break;
+                
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız!");
+                    break;
+                
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız!");
+                    break;
+                
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar ayındasınız!");
+                    break;
+                default:
+                break;
+            }
+        }
+    }
+}
