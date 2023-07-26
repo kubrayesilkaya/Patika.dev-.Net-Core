f4fe2ef84b52b0ce6228300d1e55f075c78ebb27
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-07-26 23:16:56 +0300

    Prints the even-indexed and odd-indexed characters as 2 space-separated strings on a single line

---
+++ C# String Manipulation - Separating Even and Odd-Indexed Characters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+
+        //print the even-indexed and odd-indexed characters as 2 space-separated strings on a single line
+        
+        int number_testCases= int.Parse(Console.ReadLine());
+        
+        int count=0;
+        while(count<number_testCases)
+        {
+            string input = Console.ReadLine();
+            
+            int lenghtOfInput = input.Length;
+            char[] characters = input.ToCharArray();
+            
+            for(int i=0; i<lenghtOfInput; i+=2)
+            {
+                Console.Write(characters[i]);
+            }
+            Console.Write("  ");
+            
+            for(int i=1; i<lenghtOfInput; i+=2)
+            {
+                Console.Write(characters[i]);
+            }
+            Console.WriteLine();
+            
+            count++;
+        }
+        
+
+    }
+}
