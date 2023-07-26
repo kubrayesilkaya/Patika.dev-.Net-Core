8ca3b6f3d3234cca7254180c8c536ee68308e468
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-07-26 20:57:40 +0300

    Exceptions - String to Integer

---
+++ Exceptions - String to Integer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        
+        try 
+        {
+            int i = int.Parse(S);
+            Console.WriteLine(i);
+        }
+        catch(FormatException)
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
